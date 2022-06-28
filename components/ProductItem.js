cbe51b391ae1aaa6a649012734747199f73b39c7
Wanasak Suraintaranggoon <u510610433@gmail.com>
2022-06-28 17:12:42 +0700

    add list products

---
+++ components/ProductItem.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import React from "react";
+
+const ProductItem = ({ product }) => {
+  return (
+    <div className="card">
+      <Link href={product.slug}>
+        <img src={product.image} alt={product.name} />
+      </Link>
+      <div className="p-4 flex flex-col items-center justify-center ">
+        <Link href={product.slug}>
+          <a>
+            <h2 className="text-xl">{product.name}</h2>
+          </a>
+        </Link>
+        <p>{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-btn">Add to cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Layout from "../components/Layout";
+import ProductItem from "../components/ProductItem";
+import data from "../utils/data";
 
 export default function Home() {
   return (
     <Layout title="Home Page">
-      <h1>Hello Wolrd</h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
     </Layout>
   );
 }
